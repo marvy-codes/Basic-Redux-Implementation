626bc12ed7a38bf78d42a34ae52f244fd3449e94
Tobi Aderibigbe <0xdegn@gmail.com>
2022-09-04 02:55:54 +0100

    -

--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {createStore} from 'redux';
+import {legacy_createStore as createStore} from 'redux';
 import allReducers from './reducers/allReducers'
+import { Provider } from "react-redux"
 
 const store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
